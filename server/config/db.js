5dbf54645728b2288f433b660049d92644c99935
gokulkannan55 <gokulkannan77335@gmail.com>
2026-07-19 23:59:41 +0530

    Fix database port and ignore node_modules

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,10 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT, 
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT, 
 });
 
 db.connect((err) => {
