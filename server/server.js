dbafed374b86418b362ade3c080b76cd86a500de
gokulkannan55 <gokulkannan77335@gmail.com>
2026-07-20 02:46:34 +0530

    Fix-CORS

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ const excelRoutes = require("./routes/excelRoutes");
 const voteRoutes = require("./routes/voteRoutes");
 const resultRoutes = require("./routes/resultRoutes");
 
-
 const app = express();
 
 app.use(cors({
@@ -23,11 +22,12 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
 app.use(express.json());
 
 // Test Route
 app.get("/", (req, res) => {
-    res.send("College Voting API Running 🚀");
+  res.send("College Voting API Running 🚀");
 });
 
 // Student Routes
@@ -39,16 +39,12 @@ app.use("/api/admin", excelRoutes);
 app.use("/api/student", voteRoutes);
 app.use("/api/results", resultRoutes);
 
+app.get("/test", (req, res) => {
+  res.json({ message: "Test Working" });
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
-
-
-app.get("/test", (req, res) => {
-    res.json({ message: "Test Working" });
-});
-
-
